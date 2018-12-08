722bb42d9bb09e1940db1635b7439e2569bbba23
Sanjay Sai <shachsan@hotmail.com>
2018-12-08 10:40:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+let ride;
+function scuberGreetingForFeet(ride){
+  if (ride<=400){
+    return 'This one is on me!';
+  }else if (ride > 2000 && ride <=2500) {
+    return 'I will gladly take your thirty bucks.';
+  }else if (ride > 2500) {
+    return 'No can do.';
+  }
+}
+let city;
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+let tip;
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
+}

9a1a81db09c548cda6a9b976565c75c3b724c3aa
oazzat <oazzat19@gmail.com>
2018-12-08 18:46:21 -0500

    Done.

--- index.js
+++ index.js
@@ -19,10 +19,8 @@ function switchOnCharmFromTip(tip){
   switch (tip){
     case ('generous'):
       return "Thank you so much.";
-      break;
     case ('not as generous'):
       return "Thank you.";
-      break;
     default:
       return "Bye."
   }
